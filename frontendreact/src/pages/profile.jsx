beea9f7095c45c6cbc42b91562972a6d0ccb27b1
Vikyath Kotian <vikyath301@gmail.com>
2026-08-04 20:21:54 +0530

    Frontend react new

--- frontendreact/src/pages/profile.jsx
+++ frontendreact/src/pages/profile.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import "../css/Profile.css";
+import "../css/profile.css";
 
 function Profile(){
     const [profile,setProfile]=useState(null);
