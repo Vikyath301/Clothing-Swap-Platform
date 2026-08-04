589a9a8cca37181aca1e32a9c71fceee1dda4312
Vikyath Kotian <vikyath301@gmail.com>
2026-08-04 18:56:10 +0530

    Prepare backend for deployment

--- index.js
+++ index.js
@@ -54,6 +54,8 @@ app.use(loginRoutes);
 app.use(itemDetailsRoutes);
 app.use(profileRoute);
 
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
