a2e5943155c3107e4f332d51617ff2baf29e81a2
Vikyath Kotian <vikyath301@gmail.com>
2026-08-05 18:38:53 +0530

    finall

--- backend/login.js
+++ backend/login.js
@@ -76,8 +76,8 @@ router.post("/login", async (req, res) => {
         res.cookie("token", token, {
 
             httpOnly: true,
-            secure: false,      // Change to true after deployment (HTTPS)
-            sameSite: "lax",
+            secure: true,      // Change to true after deployment (HTTPS)
+            sameSite: "none",
             maxAge: 30 * 24 * 60 * 60 * 1000
 
         });
